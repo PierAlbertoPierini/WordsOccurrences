1f4e45395f52ff3f860b7c965490943144f14bea
PierAlbertoPierini <pieralbertopierini@gmail.com>
2018-03-27 00:31:54 -0700

    Working on print only the last or first 10 words

--- main.rb
+++ main.rb
@@ -7,10 +7,23 @@ words_occurrences = text.scan(/\w+/).reduce(Hash.new(0)){|res,w| res[w.downcase]
 filtered_words ||= words_occurrences.reject do |word| filter.include?(word)
 end
 
+sorted_words_by_value = filtered_words.sort_by {|_key, value| value}.to_h
+
+reverted = filtered_words.keys.sort[0,10].each do |key|
+end
+ 
+
+puts "-"*20
 puts words_occurrences
+puts "-"*20
+puts words_occurrences.length
 puts "-"*20
 puts filtered_words
 puts "-"*20
 puts filtered_words.length
 puts "-"*20
 puts filtered_words.size
+puts "-"*20
+puts sorted_words_by_value
+puts "-"*20
+puts reverted
