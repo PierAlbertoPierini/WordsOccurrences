93b70567c0bb8737e42f32e7b4d2d9941fa82356
PierAlbertoPierini <pieralbertopierini@gmail.com>
2018-03-27 23:54:37 -0700

    Worked on longest words and highest occurring words

--- main.rb
+++ main.rb
@@ -12,6 +12,10 @@ sorted_words_by_value = filtered_words.sort_by {|_key, value| value}.to_h
 reverted = filtered_words.keys.sort[0,10].each do |key|
 end
  
+longest_words = filtered_words.inject({}) do |result, word|
+      result[word] = word.length
+      result
+    end.group_by { |key, value| value }.max_by { |key, value| key }.last
 
 puts "-"*20
 puts words_occurrences
@@ -27,3 +31,7 @@ puts "-"*20
 puts sorted_words_by_value
 puts "-"*20
 puts reverted
+puts "-"*20
+puts filtered_words.group_by { |key, value| value }.max_by { |key, value| key }.last
+puts "-"*20
+puts longest_words
